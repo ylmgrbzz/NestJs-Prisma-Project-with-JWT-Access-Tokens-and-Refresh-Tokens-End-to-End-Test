32ca69b5d128e8fe62fc875634b1e3c03eee95f2
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-28 15:47:10 +0300

    NestJs JWT Access Tokens and Refresh Tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,30 +6,45 @@ import {
     Post,
     UseGuards,
 } from '@nestjs/common';
+
+import { Public, GetCurrentUserId, GetCurrentUser } from '../common/decorators';
+import { RtGuard } from '../common/guards';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private AuthService: AuthService) { }
+    constructor(private authService: AuthService) { }
 
-    @Post('/local/signup')
+    @Public()
+    @Post('local/signup')
+    @HttpCode(HttpStatus.CREATED)
     signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
-        return this.AuthService.signupLocal(dto);
-    }
-    @Post('/local/signin')
-    signinLocal() {
-        return this.AuthService.signinLocal();
+        return this.authService.signupLocal(dto);
     }
 
-    @Post('/logout')
-    logout() {
-        return this.AuthService.logout();
+    @Public()
+    @Post('local/signin')
+    @HttpCode(HttpStatus.OK)
+    signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
+        return this.authService.signinLocal(dto);
     }
 
-    @Post('/refresh')
-    refreshTokens() {
-        return this.AuthService.refreshTokens();
+    @Post('logout')
+    @HttpCode(HttpStatus.OK)
+    logout(@GetCurrentUserId() userId: number): Promise<boolean> {
+        return this.authService.logout(userId);
+    }
+
+    @Public()
+    @UseGuards(RtGuard)
+    @Post('refresh')
+    @HttpCode(HttpStatus.OK)
+    refreshTokens(
+        @GetCurrentUserId() userId: number,
+        @GetCurrentUser('refreshToken') refreshToken: string,
+    ): Promise<Tokens> {
+        return this.authService.refreshTokens(userId, refreshToken);
     }
 }
